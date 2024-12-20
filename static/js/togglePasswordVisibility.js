1ff103a39b4e0cfc47e318406b88158f70999f65
jaypbazar <jabazar@my.cspc.edu.ph>
2024-12-20 22:49:07 +0800

    Signup page refactored and minor changes to login page

---
+++ static/js/togglePasswordVisibility.js
@@ -0,0 +1,12 @@
+const passwordInput = document.getElementById('inputPassword');
+const passwordViewBtn = document.getElementById('password-view-btn');
+
+passwordViewBtn.addEventListener('click', () => {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordViewBtn.innerHTML = '<img src="../../static/images/eye_open.png" width="30" height="20">';
+    } else {
+        passwordInput.type = 'password';
+        passwordViewBtn.innerHTML = '<img src="../../static/images/eye_close.png" width="30" height="20">';
+    }
+});
